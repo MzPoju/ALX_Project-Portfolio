b20e5945ddc9cca7459c4c3c28c1a3292755b77e
Opeyemi Adepoju <99694573+MzPoju@users.noreply.github.com>
2023-09-11 13:06:26 +0100

    Update Energy-efficiency.py

--- Project-Portfolio/Energy-efficiency.py
+++ Project-Portfolio/Energy-efficiency.py
@@ -1,10 +1,12 @@
 def calculate_energy_efficiency(energy_consumption, building_area):
     energy_efficiency = energy_consumption / building_area
     return energy_efficiency
+    """ Given parameters available are only energy consumption and building area """
 
 def main():
     energy_consumption = float(input("Enter the total energy consumption (in kWh): "))
     building_area = float(input("Enter the building area (in square meters): "))
+    """ Building area parameter is to be calculated in meter squared """
     
     energy_efficiency = calculate_energy_efficiency(energy_consumption, building_area)
     
